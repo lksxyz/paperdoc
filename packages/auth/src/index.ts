14a3b01f7c4388f447a5f66b97b5717932ee0fb7
Fahmi <fahmilukis04@gmail.com>
2026-06-10 22:21:24 +0700

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,34 @@
+import { expo } from "@better-auth/expo";
+import { createDb } from "@Paperdoc/db";
+import * as schema from "@Paperdoc/db/schema/auth";
+import { env } from "@Paperdoc/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "sqlite",
+
+      schema: schema,
+    }),
+    trustedOrigins: [env.CORS_ORIGIN, "Paperdoc://", "exp://", "http://localhost:8081"],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    advanced: {
+      defaultCookieAttributes: {
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+      },
+    },
+    plugins: [expo()],
+  });
+}
+
+export const auth = createAuth();
